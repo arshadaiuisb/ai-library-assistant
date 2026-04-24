b83c6e7026c4dbd661e1e4af4ae8b21bfe593aa8
arshadaiuisb <arshadaiuisb@gmail.com>
2026-04-24 04:49:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -82,5 +82,4 @@ if search:
 col1.metric("Total Books", len(data))
 col2.metric("Subjects", data['subject'].nunique())
 col3.metric("Authors", data['author'].nunique())
-
-    st.info("You can later connect this app with Koha API or OpenAI API for advanced answers.")
+st.info("You can later connect this app with Koha API or OpenAI API for advanced answers.")
